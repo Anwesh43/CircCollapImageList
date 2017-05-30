fae8455fa23c008f216a62b09e80b22594574f28
Anwesh43 <anweshthecool0@gmail.com>
2017-05-30 16:02:17 +0530

    using OnCollpaseListener in app

--- app/src/main/java/com/anwesome/ui/circcollapimagelistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/circcollapimagelistdemo/MainActivity.java
@@ -4,8 +4,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.anwesome.ui.circcollapimagelist.CircCollapImageList;
+import com.anwesome.ui.circcollapimagelist.OnCollapseListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +18,18 @@ public class MainActivity extends AppCompatActivity {
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.stp);
         CircCollapImageList circCollapImageList = new CircCollapImageList(this);
         for(int i=0;i<12;i++) {
-            circCollapImageList.addImage(bitmap);
+            final int index = i;
+            circCollapImageList.addImage(bitmap, new OnCollapseListener() {
+                @Override
+                public void onCollapse() {
+                    Toast.makeText(MainActivity.this, String.format("%d opened",index), Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onOpen() {
+                    Toast.makeText(MainActivity.this, String.format("%d collapsed",index), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
         circCollapImageList.show();
     }

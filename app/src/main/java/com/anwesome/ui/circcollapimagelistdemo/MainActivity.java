fecacd75741fac7b483b2e99985d94e7679bd37e
Anwesh43 <anweshthecool0@gmail.com>
2017-05-30 15:48:54 +0530

    Using CircCollapImageList in app

--- app/src/main/java/com/anwesome/ui/circcollapimagelistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/circcollapimagelistdemo/MainActivity.java
@@ -1,13 +1,23 @@
 package com.anwesome.ui.circcollapimagelistdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.anwesome.ui.circcollapimagelist.CircCollapImageList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.stp);
+        CircCollapImageList circCollapImageList = new CircCollapImageList(this);
+        for(int i=0;i<12;i++) {
+            circCollapImageList.addImage(bitmap);
+        }
+        circCollapImageList.show();
     }
 }
